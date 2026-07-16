45b3f0afdf45aed112046adbb0c25b9f679511dc
AbdulRafay-src <abdulrafay.src@gmail.com>
2026-07-16 07:37:06 -0400

    fix: exclusive yank & paste no longer needed

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,9 +3,6 @@ vim.g.maplocalleader = ' '
 
 local keymap = vim.keymap.set
 
-keymap({ 'n', 'x' }, 'gy', '"+y', { desc = 'Copy to clipboard' })
-keymap({ 'n', 'x' }, 'gp', '"+p', { desc = 'Paste clipboard text' })
-
 keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show line [E]rror diagnostics' })
 
 keymap('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
