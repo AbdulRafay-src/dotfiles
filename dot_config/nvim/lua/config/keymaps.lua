b99e34603f705eb99621ca54e1a3e786e3df2ebd
AbdulRafay-src <abdulrafay.src@gmail.com>
2026-07-16 07:32:05 -0400

    feat: add no highlight keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,5 @@ keymap({ 'n', 'x' }, 'gy', '"+y', { desc = 'Copy to clipboard' })
 keymap({ 'n', 'x' }, 'gp', '"+p', { desc = 'Paste clipboard text' })
 
 keymap('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show line [E]rror diagnostics' })
+
+keymap('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
